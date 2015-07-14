820d25d8f92826807b8a307fc2a687016868f3a2
Samuel Zaragoza <alexzm1@gmail.com>
2015-07-14 10:30:16 -0500

    Using controllerAs in the validation-error-directive.js file

--- app/scripts/directives/validation-error-directive.js
+++ app/scripts/directives/validation-error-directive.js
@@ -3,13 +3,14 @@ angular.module('appTopUp').directive('validationError', function(){
     return{
         restrict : 'E',
         controller: function($scope, $rootScope){
-        	$scope.alerts = $rootScope.alerts;
-        	$scope.close = function(index){
+        	this.alerts = $rootScope.alerts;
+        	this.close = function(index){
         		this.alerts.splice(index,1);
         	};
         },
+        controllerAs: 'validationeError',
         template: function(){
-        	return '<alert ng-repeat="alert in alerts" type="alert.type" close="close($index)">{{alert.message}}</alert>';
+        	return '<alert ng-repeat="alert in validationeError.alerts" type="alert.type" close="validationeError.close($index)">{{alert.message}}</alert>';
         }
     };
 });
